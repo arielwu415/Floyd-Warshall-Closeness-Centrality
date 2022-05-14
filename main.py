4266f0422521db70c30c0436edf85a7f6d0d0b55
Babak Shajari <babakshajar@gmail.com>
2022-05-13 17:43:14 -0700

    added produce report function and duration.

--- main.py
+++ main.py
@@ -4,38 +4,44 @@ Created on Fri May 13 10:37:26 2022
 
 @author: Ariel
 """
+import enum
 from mpi4py import MPI
 import networkx as nx
 import numpy as np
+import time
 
-# @dict:        Dictionary containing centrality scores for network nodes
 
-
-def produce_report(dict):
+def produce_report(_list, time):
     report = ''
-    for key, value in dict.items():
-        report += f"{key}:\t{value}\n"
+    for i, item in enumerate(_list):
+        report += f"node {i}:\t{item}\n"
 
     with open('output.txt', 'w', encoding="utf-8") as file:
         file.write(report)
 
+    dic = dict()
+    for i, item in enumerate(_list[0]):
+        dic[i] = item
+
+    top_keys = sorted(dic, key=dic.get, reverse=True)[:5]
+
     top_five_list = list()
-    try:
-        top_five_list = sorted(dict, key=dict.get, reverse=True)[:5]
-    except:
-        print('oops, something went wrong!')
+    for key in top_keys:
+        top_five_list.append(dic[key])
 
     print("Top five nodes: ", end='')
+
     total = 0
     for i, item in enumerate(top_five_list):
         if i == len(top_five_list) - 1:
-            total += dict[item]
+            total += item
             print(f'{item}', end='')
         else:
-            total += dict[item]
+            total += item
             print(f'{item}, ', end='')
 
     print(f'\nAverage of top five is: {total / len(top_five_list)}')
+    print(f'\n\nExecution time: {time}')
 
 
 # Communication Creation
@@ -44,40 +50,41 @@ rank = comm.Get_rank()
 size = comm.Get_size()
 
 # p = 8
-n = 4038
 # If rank 0 read data and distribute submatrix to other processors
 if rank == 0:
+    # @_list:        list containing centrality scores for network nodes
+    start_time = time.time()
+
+    # Getting info out of txt file using edgelist function
+    G = nx.read_edgelist("facebook_combined.txt",
+                         create_using=nx.DiGraph(), nodetype=int)
+
+    # Making adjacency matrix
+    A = nx.to_numpy_array(G, nonedge=8000, dtype='i')
+    # Making self node values = 0
+    A[np.diag_indices_from(A)] = 0
+    n = A.shape[0]
 
     # Rows per processor
     rows_per_proc = [int(n / size)+1 if i < n %
                      size else int(n / size) for i in range(size)]
 
-    # Getting info out of txt file using edgelist function
-    G = nx.read_edgelist("facebook_combined.txt",
-                         create_using=nx.DiGraph(), nodetype=int)
-    # Turn directed graph to undirected
-    h = G.to_directed()
-
-    # Making adjacency matrix
-    A = nx.to_numpy_array(h, nonedge=float('inf'))
-    # Making self node values = 0
-    A[np.diag_indices_from(A)] = 0
-
-    for i in range(size):
-        splitmatrix = np.array_split(A, size, axis=0)
+    splitmatrix = np.array_split(A, size, axis=0)
 
 else:
     splitmatrix = None
     rows_per_proc = None
+    n = 0
 
 
 # Scatter and broadcast data to processors
 adj_local = comm.scatter(splitmatrix, root=0)
 rows_per_proc = comm.bcast(rows_per_proc, root=0)
+n = comm.bcast(n, root=0)
 
 
 # Print what each processors data is
-print('Processor {} has data:'.format(rank), adj_local)
+print('Processor {} has data:'.format(rank), adj_local, "\nn=", n)
 
 
 # Floyd-Warshall Algorithm
@@ -126,6 +133,5 @@ C = comm.gather(Cr, root=0)
 
 # Result Output
 if rank == 0:
-    sorted_C = C.sort
-    print("----------------------------------------------------")
-    print(sorted_C[:30])
+    execution_time = time.time() - start_time
+    produce_report(C, execution_time)
